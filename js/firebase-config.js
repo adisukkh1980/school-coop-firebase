b9679838cc24cae9e72a61739292d0e6c8af4ce6
adisukkh1980 <adisukkh1980@gmail.com>
2026-05-29 23:38:44 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,21 @@
 (function () {
-  window.firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-  };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnNg5rL-pq6hR85B5wibql2NKt6HabSJA",
+  authDomain: "school-coop-firebase.firebaseapp.com",
+  projectId: "school-coop-firebase",
+  storageBucket: "school-coop-firebase.firebasestorage.app",
+  messagingSenderId: "635530533389",
+  appId: "1:635530533389:web:5c4076f80b9122dcfd5344"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
   window.isFirebaseConfigured = function isFirebaseConfigured() {
     return Boolean(
